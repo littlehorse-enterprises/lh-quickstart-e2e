bf6d28decf287330febb3840ea39fac0030c969b
Saúl Piña <saul@littlehorse.io>
2024-11-12 09:18:06 -0500

    feat: add testcontainer lib
    Co-authored-by: Eduwer Camacaro <eduwer@littlehorse.io>

--- app/src/test/java/io/littlehorse/e2e/StandaloneBootstrapper.java
+++ app/src/test/java/io/littlehorse/e2e/StandaloneBootstrapper.java
@@ -1,17 +1,23 @@
 package io.littlehorse.e2e;
 
+import io.littlehorse.container.LittleHorseCluster;
 import io.littlehorse.sdk.common.config.LHConfig;
 import io.littlehorse.test.internal.TestBootstrapper;
 
 public class StandaloneBootstrapper implements TestBootstrapper {
 
+    private final LittleHorseCluster littleHorseCluster = LittleHorseCluster.newBuilder()
+            .withInstances(2)
+            .withKafkaImage("apache/kafka-native:latest")
+            .withLittlehorseImage("ghcr.io/littlehorse-enterprises/littlehorse/lh-server:master")
+            .build();
 
     public StandaloneBootstrapper() {
-
+        littleHorseCluster.start();
     }
 
     @Override
     public LHConfig getWorkerConfig() {
-        return new LHConfig();
+        return new LHConfig(littleHorseCluster.getClientProperties());
     }
 }
