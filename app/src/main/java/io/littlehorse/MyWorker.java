383d9e4d777f33e14d0c16a474033dd06db0c91c
Saúl Piña <saul@littlehorse.io>
2025-01-13 10:13:47 -0500

    feat: add e2e test example (#1)
    Co-authored-by: Eduwer Camacaro <eduwer@littlehorse.io>

--- app/src/main/java/io/littlehorse/MyWorker.java
+++ app/src/main/java/io/littlehorse/MyWorker.java
@@ -4,14 +4,16 @@ import io.littlehorse.sdk.worker.LHTaskMethod;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static io.littlehorse.BasicExample.GREET_TASK;
+
 public class MyWorker {
 
     private static final Logger log = LoggerFactory.getLogger(MyWorker.class);
 
-    @LHTaskMethod("greet")
+    @LHTaskMethod(GREET_TASK)
     public String greeting(String name) {
         log.debug("Executing task greet");
-        return "hello there, " + name;
+        return "Hello there! " + name;
     }
 
 }
